25d7f0075c02b61c9822e128c5bbe2e9e555c190
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-23 17:24:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name=(fido)
+    @name = fido
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(type)
+    @breed = type
+  end
+
+  def breed
+    @breed    
+  end
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  def name=(dude)
+    @name = dude
+  end
+
+  def name
+    @name
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+
+  
+
+end
